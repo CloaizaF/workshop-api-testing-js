1e2d37ee8bbc0d1d4ab08beba414692b9c39318e
CloaizaF <cloaiza@unal.edu.co>
2021-02-09 14:18:48 -0500

    Starting to consume PUT Methods

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -1,3 +1,11 @@
-// const agent = require('superagent');
-// const statusCode = require('http-status-codes');
-// const { expect } = require('chai');
+const agent = require('superagent');
+const statusCode = require('http-status-codes');
+const { expect } = require('chai');
+
+describe('Consuming PUT Methods', () => {
+
+    describe('When following a user', () => {
+
+    });
+    
+});
